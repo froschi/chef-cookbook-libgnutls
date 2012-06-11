0d99993ac48a825913680a14131ae895ba48cf54
Thorsten Fischer <thorsten@froschi.org>
2012-06-11 13:49:38 +0200

    Amended for use on lucid, precise.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,14 @@
 include_recipe "libgcrypt"
 include_recipe "libtasn"
 
-packages = %w/
-  libgnutls26
-/
+packages = Array.new
+
+case node[:lsb][:codename]
+when "lucid", "precise"
+  packages |= %w/
+    libgnutls26
+  /
+end
 
 packages.each do |pkg|
   package pkg do
